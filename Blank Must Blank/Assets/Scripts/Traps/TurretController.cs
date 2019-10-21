2fb8b9502fdfee26d314b05aa03aa79f3ace4736
José Pedro Borges <up201503603@fe.up.pt>
2019-10-21 17:58:57 +0100

    turret follows robots

--- Blank Must Blank/Assets/Scripts/Traps/TurretController.cs
+++ Blank Must Blank/Assets/Scripts/Traps/TurretController.cs
@@ -6,16 +6,23 @@ public class TurretController : MonoBehaviour {
     private List<GameObject> targets = new List<GameObject>();
     public GameObject headObject;
 
+    public Transform partToRotate;
+
     private void Start() {
         StartCoroutine(Fire());
     }
 
     private void Update() {
         if (this.targets.Count > 0) {
-            Transform target = this.targets[0].transform;
+            /*Transform target = this.targets[0].transform;
             Vector3 direction = (target.position - headObject.transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(direction);
-            headObject.transform.rotation = Quaternion.Slerp(headObject.transform.rotation, lookRotation, Time.deltaTime * 2);
+            headObject.transform.rotation = Quaternion.Slerp(headObject.transform.rotation, lookRotation, Time.deltaTime * 2); */
+
+            Vector3 dir = this.targets[0].transform.position - this.transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = lookRotation.eulerAngles;
+            this.partToRotate.rotation =  Quaternion.Euler(0f, rotation.y, 0f);
         }
     }
 
