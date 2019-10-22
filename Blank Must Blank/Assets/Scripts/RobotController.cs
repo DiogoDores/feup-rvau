f9f4c5e2a0cd48f31209d03667954d869ba04120
Miguel Fernandes <miguelmanofernandes@gmail.com>
2019-10-22 20:03:43 +0100

    Improve turret AI based on proximity

--- Blank Must Blank/Assets/Scripts/RobotController.cs
+++ Blank Must Blank/Assets/Scripts/RobotController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class RobotController : MonoBehaviour {
     public Transform riftTransform;
-    public float robotHP = 2;
+    public float health = 100;
+
+    [Header("UI")]
+    public Image healthBar;
 
     private void Start() {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = this.riftTransform.position;
+    }
+
+    private void Update() {
+        if (this.health <= 0) Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -17,4 +25,11 @@ public class RobotController : MonoBehaviour {
             Destroy(gameObject);
         }
     }
+
+    public void TakeDamage(float amount) {
+        this.health -= amount;
+        this.healthBar.fillAmount = this.health / 100.0f;
+    }
+
+    public float GetHealth() => this.health;
 }
--- Blank Must Blank/Assets/Scripts/Traps/TurretController.cs
+++ Blank Must Blank/Assets/Scripts/Traps/TurretController.cs
@@ -3,52 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretController : MonoBehaviour {
-    private List<GameObject> targets = new List<GameObject>();
-    public GameObject headObject;
+    public float damage;
+    public Transform head;
 
-    public Transform partToRotate;
+    private GameObject target;
+    private float lockSpeed = 2.0f;
 
     private void Start() {
         StartCoroutine(Fire());
     }
 
     private void Update() {
-        if (this.targets.Count > 0) {
-            /*Transform target = this.targets[0].transform;
-            Vector3 direction = (target.position - headObject.transform.position).normalized;
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-            headObject.transform.rotation = Quaternion.Slerp(headObject.transform.rotation, lookRotation, Time.deltaTime * 2); */
-
-            Vector3 dir = this.targets[0].transform.position - this.transform.position;
+        if (target != null) {
+            Vector3 dir = this.target.transform.position - this.transform.position;
             Quaternion lookRotation = Quaternion.LookRotation(dir);
             Vector3 rotation = lookRotation.eulerAngles;
-            this.partToRotate.rotation =  Quaternion.Euler(0f, rotation.y, 0f);
+            head.rotation = Quaternion.Slerp(this.head.rotation, Quaternion.Euler(0f, rotation.y, 0f), Time.deltaTime * this.lockSpeed);
         }
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Robot")) {
-            this.targets.Add(other.gameObject);
-        }
-    }
+    private GameObject FindClosestRobot() {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 7.0f);
+        float minDistance = Mathf.Infinity;
+        GameObject nearest = null;
 
-    private void OnTriggerExit(Collider other) {
-        if (other.CompareTag("Robot")) {
-            this.targets.Remove(other.gameObject);
-        }
+        for (int i = 0; i < colliders.Length; i++) {
+            if (colliders[i].gameObject.tag != "Robot") {
+                continue;
+            }
+
+            float thisDistance = (transform.position - colliders[i].transform.position).sqrMagnitude;
+            
+            if (thisDistance < minDistance) {
+                minDistance = thisDistance;
+                nearest = colliders[i].gameObject;
+            }
+        }  
+
+        return nearest;
     }
 
     private IEnumerator Fire() {
         while (true) {
-            if (this.targets.Count > 0) {
-                this.targets[0].GetComponent<RobotController>().robotHP--;
-                Debug.Log(this.targets[0].GetComponent<RobotController>().robotHP);
-                if (this.targets[0].GetComponent<RobotController>().robotHP == 0){
-                    Destroy(this.targets[0]);
-                    this.targets.RemoveAt(0);
-                }
+            this.target = FindClosestRobot();
+            print(this.target);
+            if (this.target != null) {
+                this.target.GetComponent<RobotController>().TakeDamage(this.damage);
             }
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.5f);
         }
     }
 }
