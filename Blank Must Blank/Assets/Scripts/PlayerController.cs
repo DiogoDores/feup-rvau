1230bad7f8e6798b458e93efa132b756244526f8
aquelemiguel <miguelmanofernandes@gmail.com>
2019-10-31 00:46:53 +0000

    Update database and start shop logic

--- Blank Must Blank/Assets/Scripts/PlayerController.cs
+++ Blank Must Blank/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     private CanvasController canvasController;
     private int cogs;
+    private Dictionary<Trap, int> markers;
+
+    private ShopController shopController;
+
+    public enum Trap {
+        SPIKES, ACID, TURRET
+    }
 
     private void Start() {
+        this.markers = new Dictionary<Trap, int>();
+
+        List<Trap> traps = new List<Trap>{ Trap.SPIKES, Trap.ACID, Trap.TURRET };
+        traps.ForEach(trap => this.markers.Add(trap, 0));
+
         this.canvasController = GameObject.FindGameObjectWithTag("Canvas").GetComponent<CanvasController>();
+        this.shopController = GameObject.FindGameObjectWithTag("Shop").GetComponent<ShopController>();
+    }
+
+    public void ShopTrap(Trap trap, bool isPurchase) {
+        if (isPurchase && this.markers[trap] < 5) 
+            this.markers[trap]++;
+
+        if (!isPurchase && this.markers[trap] > 0)
+            this.markers[trap]--;
+
+        this.shopController.UpdateInventoryLabel(trap, this.markers[trap]);
     }
 
     public void CollectCogs(int amount) {
---
+++ Blank Must Blank/Assets/Scripts/UI/ShopController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour {
+    private PlayerController playerController;
+    public Text invLabelSpikes, invLabelTurret, invLabelAcid;
+
+    private void Start() {
+        this.playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    public void UpdateInventoryLabel(PlayerController.Trap trap, int value) {
+        if (trap == PlayerController.Trap.ACID) {
+            this.invLabelAcid.text = value.ToString() + "/5";
+        }
+        else if (trap == PlayerController.Trap.TURRET) {
+            this.invLabelTurret.text = value.ToString() + "/5";
+        }
+        else if (trap == PlayerController.Trap.SPIKES) {
+            this.invLabelSpikes.text = value.ToString() + "/5";
+        }
+    }
+
+    public void PurchaseTrap(string trap) {
+        System.Enum.TryParse(trap, out PlayerController.Trap trapEnum);
+        playerController.ShopTrap(trapEnum, true);
+    }
+
+    public void SellTrap(string trap) {
+        System.Enum.TryParse(trap, out PlayerController.Trap trapEnum);
+        playerController.ShopTrap(trapEnum, false);
+    }
+}
