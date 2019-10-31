b91a99e6bd9a5275ab11d7b0a52517fc88f308fd
aquelemiguel <miguelmanofernandes@gmail.com>
2019-10-31 03:17:18 +0000

    Disable image targets on scene start

--- Blank Must Blank/Assets/Scripts/PlayerController.cs
+++ Blank Must Blank/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour {
         [HideInInspector] public int lastMarker;
 
         public Trap(Type type, int cost) {
+            this.targets = new List<GameObject>();
             this.type = type;
             this.cost = cost;
             this.lastMarker = 0;
@@ -32,6 +33,8 @@ public class PlayerController : MonoBehaviour {
     private void Start() {
         this.canvasController = GameObject.FindGameObjectWithTag("Canvas").GetComponent<CanvasController>();
         this.shopController = GameObject.FindGameObjectWithTag("Shop").GetComponent<ShopController>();
+
+        StartCoroutine(DisableImageTargets());
     }
 
     public void ShopTrap(Trap.Type trapType, bool isPurchase) {
@@ -45,16 +48,23 @@ public class PlayerController : MonoBehaviour {
         if (isPurchase && trap.lastMarker < 5) {
             trap.lastMarker++;
             this.cogs -= trap.cost;
-            trap.targets[trap.lastMarker - 1].SetActive(true);
+            trap.targets[trap.lastMarker - 1].GetComponent<ImageTargetBehaviour>().enabled = true;
         }
 
         if (!isPurchase && trap.lastMarker > 0) {
             trap.lastMarker--;
             this.cogs += trap.cost;
-            trap.targets[trap.lastMarker].SetActive(false);
+            trap.targets[trap.lastMarker].GetComponent<ImageTargetBehaviour>().enabled = false;
         }
 
         this.shopController.UpdateInventoryLabel(trap.type, trap.lastMarker);
+    }
+
+    private IEnumerator DisableImageTargets() {
+        yield return new WaitForSeconds(0.1f);
+        this.traps.ForEach(trap => trap.targets.ForEach(target => {
+            target.GetComponent<ImageTargetBehaviour>().enabled = false;
+        }));
     }
 
     public int GetCogs() {
