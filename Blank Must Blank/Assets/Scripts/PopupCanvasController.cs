fbd6849781decbc09316e70cb4dda73a4f2f4031
aquelemiguel <miguelmanofernandes@gmail.com>
2019-11-03 22:30:04 +0000

    Add help button

---
+++ Blank Must Blank/Assets/Scripts/PopupCanvasController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupCanvasController : MonoBehaviour {
+    public Text hintText;
+    private int currentHint = 0;
+    public List<string> hints;
+
+    private void Awake() {
+        this.currentHint = 0;
+        this.hintText.text = this.hints[this.currentHint];
+    }
+
+    public void NextHint() {
+        if (this.currentHint < this.hints.Count - 1) {
+            this.hintText.text = this.hints[++this.currentHint];
+        } else {
+            this.currentHint = 0;
+            gameObject.SetActive(false);
+        }
+    }
+}
