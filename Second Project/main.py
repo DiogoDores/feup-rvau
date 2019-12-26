d0e0b3b185e6f09c930fcda9be8ab67eddde350d
Diogo Dores <diogo.dores@gmail.com>
2019-12-26 15:20:35 +0000

    Added free kick functionality and argument parser

--- Second Project/main.py
+++ Second Project/main.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+import argparse
 from utils import *
 
 import sys
@@ -28,7 +29,8 @@ def get_blue_points(img):
 
     blue_points = []
 
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(
+        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
@@ -46,25 +48,33 @@ def get_blue_points(img):
 
 if __name__ == '__main__':
 
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-m", "--mode", type=int, required=True,
+                    help="Which mode to use: 1 - Offside Player; 2 - Free kick; 3 - Distance to goal;")
+    ap.add_argument("-i", "--image", required=True,
+                    help="Source Image Path")
+    args = vars(ap.parse_args())
+
+    mode = args["mode"]
+    im_src = args["image"]
+
     # Fetch blue points.
     im_dst = cv2.imread('plane_field.png')
     blue_points = get_blue_points(im_dst)
     pts_dst = np.array(blue_points, dtype=float)
-    print(pts_dst)
 
     # Read source image.
-    im_src = cv2.imread('field.jpg')
+    im_src = cv2.imread(im_src)
     size = im_src.shape
 
     # Get four corners of the field
     print('Select the homography points of the field and then press ENTER')
     pts_src = get_field_points(im_src)
-    print(pts_src)
 
     # Get offside player
     print('Select the offside player and then press ENTER')
     player = get_player(im_src)
-    print(player)
 
     height = int(im_dst.shape[0] * 20 / 100)
     width = int(im_dst.shape[1] * 20 / 100)
@@ -85,14 +95,22 @@ if __name__ == '__main__':
     # Add warped source image to destination image.
     im_dst = im_dst + im_temp
 
-    # Display image.
-    cv2.imshow("Image", im_dst)
-
     # Convert player based on perspective transform.
     player_new = cv2.perspectiveTransform(np.array([player]), h)
-    player_new = [[(player_new[0][0][0]), 0.0], [(player_new[0][0][0]), size[0]]]
-
     print(player_new)
+
+    if mode == 1:
+        player_new = [[(player_new[0][0][0]), 0.0], [
+            (player_new[0][0][0]), size[0]]]
+        print(player_new)
+    elif mode == 2:
+        x = (player_new[0][0][0])
+        y = (player_new[0][0][1])
+
+        delta = abs(pts_dst[6][0] - pts_dst[7][0])
+        radius_px = delta * 9.15 / 16.5
+        player_new = [[x - radius_px, y - radius_px], [x + radius_px, y - radius_px],
+                      [x + radius_px, y + radius_px], [x - radius_px, y + radius_px]]
 
     h_inv, status = cv2.findHomography(pts_dst, pts_src)
     player_best = cv2.perspectiveTransform(np.array([player_new]), h_inv)
@@ -104,11 +122,20 @@ if __name__ == '__main__':
     #im_temp = cv2.warpPerspective(im_dst, h_inv, (im_src.shape[1], im_src.shape[0]))
     #im_src = im_temp
 
-    player_p1, player_p2 = player_best[0].astype(int)
-    print(player_p1, player_p2)
+    if mode == 1:
+        player_p1, player_p2 = player_best[0].astype(int)
+        print(player_p1, player_p2)
 
-    
-    im_dst = cv2.line(im_src, (player_p1[0], player_p1[1]), (player_p2[0], player_p2[1]), (255, 0, 0), 4)
-    cv2.imshow("Image", im_src)
+        im_name = "Offside"
+        im_dst = cv2.line(
+            im_src, (player_p1[0], player_p1[1]), (player_p2[0], player_p2[1]), (255, 0, 0), 2)
+    elif mode == 2:
+        player_best = player_best[0].astype(int)
+        box = cv2.minAreaRect(player_best)
+
+        im_name = "Free kick"
+        im_dst = cv2.ellipse(im_src, box, (255, 0, 0), 2)
+
+    cv2.imshow(im_name, im_src)
 
     cv2.waitKey(0)
